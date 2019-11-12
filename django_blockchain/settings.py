c650cc5c35477a39b9e3a7b51fd8db7e63d97b59
criptoanarquist <alejoveintimilla@gmail.com>
2019-11-11 23:08:41 -0500

    gitignore

--- django_blockchain/settings.py
+++ django_blockchain/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blocks',
+    'django_blockchain',
+    'transactions',
+    'wallet'
 ]
 
 MIDDLEWARE = [
